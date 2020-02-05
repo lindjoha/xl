5f8f98e4f05943e16eb329724067d9af4226579a
Øyvind Lind-Johansen (OTE PNF JC) <olind@st-linrgs118.st.statoil.no>
2020-02-05 12:58:50 +0100

    another commit

--- xl/__init__.py
+++ xl/__init__.py
@@ -1,6 +1,11 @@
+import pandas as pd
 
-def main()
+def read_excel_file(fn):
+	return pd.read_excel(fn)
+
+def main():
 	import sys
 	fn = sys.argv[1]
-	print(fn)
-	
+	df = read_excel_file(fn)
+	columns = list(df.columns)
+	print("\n".join(columns))
